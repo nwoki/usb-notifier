203fe61052487d0b3394cc3ab5d039f26b31f09f
Francesco Nwokeka <francesco.nwokeka@gmail.com>
2015-03-31 14:30:09 +0200

    test example outputs device info

--- example/testconnect.cpp
+++ example/testconnect.cpp
@@ -3,17 +3,22 @@
 #include <UsbNotifier/UsbDevice>
 #include <UsbNotifier/UsbNotifier>
 
+#include <QtCore/QDebug>
+
 #include <libusb-1.0/libusb.h>
 
 TestConnect::TestConnect(QObject *parent)
     : QObject(parent)
     , m_usb(new UsbNotifier::UsbNotifier(0, 0, this))
 {
-    m_usb->start();
-
     connect(m_usb, &UsbNotifier::UsbNotifier::deviceAttached, [] (UsbNotifier::UsbDevice *dev) {
         qDebug("[TestConnect::TestConnect] GOT A DEVICE");
+
+        qDebug() << "MANUFACTURER: " << dev->manufacturer();
+        qDebug() << "PRODUCT: " << dev->product();
     });
+
+    m_usb->start();
 }
 
 TestConnect::~TestConnect()
